c7122286ad702cfda93bfc04ea272cde193379c3
rate <rate98326@gmail.com>
2024-06-01 21:00:09 +0900

    wchar_tを使うように修正

--- main.c
+++ main.c
@@ -5,98 +5,107 @@
 #include <locale.h>
 #include <wchar.h>
 
-char roboks[] =
-    "    w\\\n"
-    "     \\          y>/一<\\w\n"
-    "      \\         y/ヽ_ノヽw\n"
-    "       \\           yﾊw\n"
-    "        \\       c———————w\n"
-    "         \\.  c／         ＼w\n"
-    "           c／  w●c       w●c  ＼w\n"
-    "          c/     w❘ニニニ❘c     c\\w\n"
-    "        c／|—————————————————|ヽw\n"
-    "       c/ /b|    g〇 y〇 m〇     b|ヽヽw\n"
-    "     r(一) b|                 | r(一)w\n"
-    "          b|     y／一＼      b|\n"
-    "          b|     y| ?  |      b|\n"
-    "          b|     y＼一／      b|\n"
-    "          b|＿  ＿＿＿＿  ＿.|\n"
-    "             cΠ         Π   \n"
-    "        r(ニニ|         |ニニ)d)";
+#define MAX_STRARG_NUM 32
 
-int mb_strlen( const char* str ) {
-    setlocale( LC_ALL, "" );
+wchar_t roboks[] = L"    w\\\n"
+                   L"     \\          y>/一<\\w\n"
+                   L"      \\         y/ヽ_ノヽw\n"
+                   L"       \\           yﾊw\n"
+                   L"        \\       c———————w\n"
+                   L"         \\.  c／         ＼w\n"
+                   L"           c／  w●c       w●c  ＼w\n"
+                   L"          c/     w❘ニニニ❘c     c\\w\n"
+                   L"        c／|—————————————————|ヽw\n"
+                   L"       c/ /b|    g〇 y〇 m〇     b|ヽヽw\n"
+                   L"     r(一) b|                 | r(一)w\n"
+                   L"          b|     y／一＼      b|\n"
+                   L"          b|     y| ?  |      b|\n"
+                   L"          b|     y＼一／      b|\n"
+                   L"          b|＿  ＿＿＿＿  ＿.|\n"
+                   L"             cΠ         Π   \n"
+                   L"        r(ニニ|         |ニニ)d)\n";
+
+int mb_strlen(const wchar_t *str) {
     int len = 0;
-    wchar_t wc;
-    mbstate_t mbs;
-    memset( &mbs, 0, sizeof( mbs ) );
-    while ( *str != '\0' ) {
-        size_t bytes = mbrtowc( &wc, str, MB_CUR_MAX, &mbs );
-        if ( bytes == (size_t)-1 || bytes == (size_t)-2 )
-            break;
-        str += bytes;
-        len += wcwidth( wc );
+    while (*str != L'\0') {
+        len += wcwidth(*str);
+        str++;
     }
     return len;
 }
 
-void say_bubble( const char* str ) {
-    int len = mb_strlen( str );
-
-    printf( "  " );
-    for ( int i = 0; i < len + 2; i++ ) {
-        printf( "-" );
+void say_bubble(const wchar_t *str) {
+    void print_bar(int len) {
+        wprintf(L"  ");
+        for (int i = 0; i < len + 2; i++) {
+            wprintf(L"-");
+        }
+        wprintf(L"\n");
     }
-    puts( "" );
-    printf( "＜" );
-    printf( " %s ＞\n", str );
 
-    printf( "  " );
-    for ( int i = 0; i < len + 2; i++ ) {
-        printf( "-" );
-    }
-    puts( "" );
+    int len = mb_strlen(str);
+
+    print_bar(len + 2);
+
+    wprintf(L"＜ %ls ＞\n", str);
+
+    print_bar(len + 2);
 }
 
-int main( int argc, char** argv ) {
-    const char* default_str = "踏めば助かるのに...";
-    char* str = ( argc == 2 ) ? argv[1] : (char*)default_str;
+int main(int argc, char **argv) {
+    const wchar_t *default_str = L"踏めば助かるのに...";
+    wchar_t *str;
+    wchar_t argbuf[MAX_STRARG_NUM];
 
-    FILE* out = stdout;
+    setlocale(LC_ALL, "");
 
-    fprintf(out, "\x1b[37m");
-    say_bubble( str );
-    char* roboks_ptr = roboks;
+    if (argc == 2) {
+        int ret = mbstowcs(argbuf, argv[1], MAX_STRARG_NUM);
+        if (ret == (size_t)-1) {
+            wprintf(L"Invalid argument\n");
+            exit(1);
+        }
+        str = argbuf;
 
-    while (*(roboks_ptr++) != '\0') {
-        int c = (int)*roboks_ptr;
-        switch(c){
-            case 'c':
-                fprintf(out, "\x1b[36m");
-                break;
-            case 'y':
-                fprintf(out, "\x1b[33m");
-                break;
-            case 'w':
-                fprintf(out, "\x1b[37m");
-                break;
-            case 'r':
-                fprintf(out, "\x1b[31m");
-                break;
-            case 'g':
-                fprintf(out, "\x1b[32m");
-                break;
-            case 'b':
-                fprintf(out, "\x1b[34m");
-                break;
-            case 'd':
-                fprintf(out, "\x1b[39m");
-                break;
-            case 'm':
-                fprintf(out, "\x1b[35m");
-                break;
-            default:
-                putc(c, out);
+    } else {
+        str = (wchar_t *)default_str;
+    }
+
+    FILE *out = stdout;
+
+    fwprintf(out, L"\x1b[37m");
+    say_bubble(str);
+    wchar_t *roboks_ptr = roboks;
+
+    while (*(roboks_ptr++) != L'\0') {
+        wchar_t c = *roboks_ptr;
+        switch (c) {
+        case L'c':
+            fwprintf(out, L"\x1b[36m");
+            break;
+        case L'y':
+            fwprintf(out, L"\x1b[33m");
+            break;
+        case L'w':
+            fwprintf(out, L"\x1b[37m");
+            break;
+        case L'r':
+            fwprintf(out, L"\x1b[31m");
+            break;
+        case L'g':
+            fwprintf(out, L"\x1b[32m");
+            break;
+        case L'b':
+            fwprintf(out, L"\x1b[34m");
+            break;
+        case L'd':
+            fwprintf(out, L"\x1b[39m");
+            break;
+        case L'm':
+            fwprintf(out, L"\x1b[35m");
+            break;
+        default:
+            putwc(c, out);
         }
     }
 
